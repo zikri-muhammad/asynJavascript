37e271d757750b6f458b2ddb07e43129e9f22042
zikri.wetalk <zikri.wetalk@gmail.com>
2023-01-30 14:24:43 +0700

    promise

---
+++ app.js
@@ -0,0 +1,18 @@
+const asynC = () => {
+    return promise = new Promise((resolve, reject) => {
+        let number = 1;
+        if (number > 0) {
+            resolve("ok")
+        } else {
+            reject("error")
+        }
+    }).then((value) => {
+        // console.log(value);
+        return value
+    })
+}
+
+
+
+console.log(await asynC())
+
